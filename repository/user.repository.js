fd016956c1b6532813a21cf1fd18289081999941
allan raymond <raymonda916@gmail.com>
2025-11-20 12:12:19 +0300

    Add repository directory

---
+++ repository/user.repository.js
@@ -0,0 +1,34 @@
+
+const User = require('../models/user.model')
+const bcrypt = requre('bcrypt')
+
+const create = async(userData) => {
+    const user = User(userData)
+    const hashedPassword = await bcrypt.hash(user.password,10)
+    user.password = hashedPassword
+    return user.save()
+}
+
+const findById = async(id) => {
+    await User.findById(id)
+}
+
+const findByEmail = async(email) => {
+    await User.findOne({email})
+}
+
+const updateData = async(id,updateData) => {
+    await findByIdAndUpdate(id, updateData, {new:true})
+}
+
+const deleteData = async(id) => {
+    await User.findByIdAndDelete(id)
+}
+
+module.exports = {
+    create,
+    findById,
+    findByEmail,
+    updateData,
+    deleteData
+}
