4ed56d2b0c5fe9ce21b3e15ff59354a8bdf356d7
allan raymond <raymonda916@gmail.com>
2025-11-20 12:12:19 +0300

    Modify server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config()
 const express = require("express")
+const connectDB = require("./db")
 const app = express()
 const port = process.env.PORT
 
-app.use(express.json()) // ensures that json objects are recognied
+app.use(express.json()) // ensures that json objects are recognized
+connectDB()
+
 
 app.get('/', (req,res) => {
     res.send("This awesome Library API is running")
