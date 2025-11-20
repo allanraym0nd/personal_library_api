6c9c2eaf2a3b1bdd9998b9403685a9a0a5c1a3da
allan raymond <raymonda916@gmail.com>
2025-11-20 12:12:19 +0300

    Add db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+require('dotenv').config
+const mongoose =require('mongoose')
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.DB_URI)
+        console.log('Database Conected Successfully')
+
+    }catch(err){
+        console.log(`A database connection error occurred: ${err.message}`)
+    }
+}
+
+module.exports = connectDB()
