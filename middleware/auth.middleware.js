114b32db726d26e97a32cf6143cd7e315c0228c9
allan raymond <raymonda916@gmail.com>
2025-11-22 20:59:54 +0300

    Add middleware directory

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+// allow only authorized users access your library API.
+
+const jwt = require('jsonwebtoken') 
+
+const authMiddleware = async(req,res,next) => {
+    const authHeader = req.header.authorization
+
+    if(!authHeader || !authHeader.startsWith("Bearer")){
+        return res.status().json({error: "Unauthorized. Token not provided"})
+    }
+
+    const authToken = authHeader.split(" ")[1]
+
+    try {
+        const decoded = jwt.verify(authToken, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    }catch(err){
+        res.status(401).json({error: "Invalid or expired token"})
+    }
+
+}
+
+module.exports = {
+    authMiddleware
+}
