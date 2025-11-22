4f0f142d9ce2a88dcf3461439df94bb3ed047c8f
allan raymond <raymonda916@gmail.com>
2025-11-22 20:59:54 +0300

    Modify server.js

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const express = require("express")
 const connectDB = require("./db")
 const app = express()
 const port = process.env.PORT
+const userRoutes = require('./routes/user.routes')
+const booksRoutes = require('./routes/books.routes')
+const swagger = require('./swagger')
+const setUpSwagger = require("./swagger")
+setUpSwagger(app)
+
 
 app.use(express.json()) // ensures that json objects are recognized
 connectDB()
 
+app.use('/users', userRoutes)
+app.use('/books', booksRoutes)
 
 app.get('/', (req,res) => {
     res.send("This awesome Library API is running")
